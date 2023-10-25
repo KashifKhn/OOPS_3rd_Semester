f67ec4961bc4d4f63b8a37acab4b5397ba39ca76
Kashif Khan <KashifKhnx04@gmail.com>
2023-10-25 17:52:33 +0500

    Add question

--- AirLineReservationSystem/src/ReservationSystem.java
+++ AirLineReservationSystem/src/ReservationSystem.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ReservationSystem {
-    private int [] seats = new int[5];
+    private int [] seats = new int[10];
     private boolean [] isReserved = new boolean[10];
-    
+
+    public ReservationSystem(int[] seats, boolean[] isReserved) {
+        this.seats = seats;
+        this.isReserved = isReserved;
+    }
+
+    public int[] getSeats() {
+        return seats;
+    }
+
+    public boolean[] getIsReserved() {
+        return isReserved;
+    }
+
+    public int displayMenu() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter\n 1 for FirstClass\n 2 for Economy .................");
+        return  sc.nextInt();
+    }
+    public void  assignSeat() {
+        int choice = displayMenu();
+        if(choice == 1) {
+
+        }
+    }
 }
